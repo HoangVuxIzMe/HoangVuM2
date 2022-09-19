45b146a404f0efede991f3c981a5179785f723ae
HoangVuxIzMe <vuhk0909@gmail.com>
2022-09-19 09:23:12 +0700

    Done

---
+++ src/Module2/bai_tap/src/ss7_abstract_class_interface/resizeable/RunShape.java
@@ -0,0 +1,19 @@
+package ss7_abstract_class_interface.resizeable;
+
+public class RunShape {
+    public static void main(String[] args) {
+        int x = (int) Math.floor(Math.random() * 10);
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle(x, "yellow", false);
+        shapes[1] = new Rectangle(x, x, "blue", true);
+        shapes[2] = new Square(x, "red", true);
+        for (Shape a : shapes) {
+            System.out.println(a);
+        }
+        for (Shape a : shapes) {
+            a.resize(Math.random() * 1000);
+        }
+        System.out.println("\n");
+        Shape.printShape(shapes);
+    }
+}
---
+++ src/Module2/bai_tap/src/ss7_abstract_class_interface/resizeable/Square.java
@@ -0,0 +1,50 @@
+package ss7_abstract_class_interface.resizeable;
+
+public class Square extends Shape {
+    private double side = 1.0;
+
+    public Square() {
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(color, filled);
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double getArea() {
+        return side * side;
+    }
+
+    public double getPerimeter() {
+        return side * 4;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString()
+                + ", Area= "
+                + getArea()
+                + ", Perimeter= "
+                + getPerimeter();
+    }
+
+    @Override
+    public void resize(double percent) {
+        this.side *= (percent / 200);
+    }
+}
