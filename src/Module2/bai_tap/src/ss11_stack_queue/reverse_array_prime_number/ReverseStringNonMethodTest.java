3f161dc6cccb05446cd03bef66ff1e9d54e70642
HoangVuxIzMe <vuhk0909@gmail.com>
2022-10-10 19:11:46 +0700

    Done

--- src/Module2/bai_tap/src/ss11_stack_queue/reverse_array_prime_number/ReverseStringNonMethodTest.java
+++ src/Module2/bai_tap/src/ss11_stack_queue/reverse_array_prime_number/ReverseStringNonMethodTest.java
@@ -1,5 +1,22 @@
 package ss11_stack_queue.reverse_array_prime_number;
 
-public class ReverseStringNonMethodTest {
+import java.util.Scanner;
+import java.util.Stack;
 
+public class ReverseStringNonMethodTest {
+    public static void main(String[] args) {
+        Stack mStack = new Stack<>();
+        Stack wStack = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your type: ");
+        String s1 = sc.nextLine();
+
+        for (int i = 0; i < s1.length(); i++) {
+            mStack.push(s1.charAt(i));
+        }
+        while(!mStack.isEmpty()){
+            wStack.push(mStack.pop());
+        }
+        System.out.println(wStack);
+    }
 }
